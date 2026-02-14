4b7f0f7fb2f058b780f6c492389bb201df67c37e
foxit64 <foxit64@users.noreply.github.com>
2026-02-14 18:48:52 +0100

    cleanup

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,28 +2,16 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
+# Install packages
 dnf5 install -y \
   btop \
   htop \
+  mc \
   nmap \
   nvim \
-  tmux
+  tmux \
+  && dnf clean all \
+  && rm -rf /var/cache/yum
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
+# Example for enabling a System Unit File
 systemctl enable podman.socket
