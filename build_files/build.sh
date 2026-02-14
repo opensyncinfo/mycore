4803a8ae28182152eb3b977ba19048f68fd3f452
foxit64 <foxit64@users.noreply.github.com>
2026-02-14 16:24:17 +0100

    Initial setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+  btop
+  htop \
+  nmap \
+  nvim \
+  tmux
 
 # Use a COPR Example:
 #
